c52c1a3f641dcfab988a95c9e8c5d5fbfbfe9ade
Raymond Pete <rpete@broadinstitute.org>
2015-08-11 13:54:12 -0400

    readd deleted token variable for consul template

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,16 @@ sed -i "s;%%CONSUL_IP%%;${CONSUL_IP};" "/etc/consul-template.conf"
 
 
 cat /dev/shm/.token | tr -d '\n' > /root/.vault-token
-APPNAME=${APPNAME:-apptest}
-POLICY=${POLICY:-none}
-APPID=$(uuidgen)
+export VAULT_TOKEN=`cat /root/.vault-token`
+export APPNAME=${APPNAME:-apptest}
+export POLICY=${POLICY:-none}
+export APPID=$(uuidgen)
 if [ -z ${ENVIRONMENT} ]; then ENVIRONMENT=ci; fi
 vault write auth/app-id/map/app-id/${APPID} value=${POLICY} display_name=${APPNAME}
 vault write auth/app-id/map/user-id/${APPNAME} value=${APPID}
 echo ${APPID}
-TOKEN=`curl -s --data "{\"app_id\":\"${APPID}\",\"user_id\":\"${APPNAME}\"}" ${VAULT_ADDR}/v1/auth/app-id/login | \
+export TOKEN=`curl -s --data "{\"app_id\":\"${APPID}\",\"user_id\":\"${APPNAME}\"}" ${VAULT_ADDR}/v1/auth/app-id/login | \
          sed -e 's/^.*"client_token":"\([^"]*\)".*$/\1/'`
 echo ${TOKEN}
 echo -n "${TOKEN}" > ~/.vault-token
+unset VAULT_TOKEN
