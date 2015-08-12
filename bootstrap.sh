4cbe0459ed2fd657df51a61b842c756135dc4a57
root <root@devops-dev01.broadinstitute.org>
2015-08-11 21:07:25 -0400

    try vault token variable and delete tokens

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,15 @@ sed -i "s;%%CONSUL_IP%%;${CONSUL_IP};" "/etc/consul-template.conf"
 
 
 cat /dev/shm/.token | tr -d '\n' > /root/.vault-token
+export VAULT_TOKEN=`cat /dev/shm/.token | tr -d '\n'`
 export APPNAME=${APPNAME:-apptest}
 export POLICY=${POLICY:-none}
 export APPID=$(uuidgen)
 if [ -z ${ENVIRONMENT} ]; then ENVIRONMENT=ci; fi
 vault write auth/app-id/map/app-id/${APPID} value=${POLICY} display_name=${APPNAME}
 vault write auth/app-id/map/user-id/${APPNAME} value=${APPID}
+rm ~/.vault-token
+unset VAULT_TOKEN
 echo ${APPID}
 export TOKEN=`curl -s --data "{\"app_id\":\"${APPID}\",\"user_id\":\"${APPNAME}\"}" ${VAULT_ADDR}/v1/auth/app-id/login | \
          sed -e 's/^.*"client_token":"\([^"]*\)".*$/\1/'`
